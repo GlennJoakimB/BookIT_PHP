6ae2f3151b5dc81670c325f7d9628efa759d96ba
Trivinyx <tom.a.s.myre@gmail.com>
2023-11-28 23:30:37 +0100

    Added rule password strength for new reg. and validation

--- BookITweb/core/Model.php
+++ BookITweb/core/Model.php
@@ -21,6 +21,7 @@ namespace app\core
         public const RULE_MATCH = 'match';
         public const RULE_UNIQUE = 'unique';
         public const RULE_USER_EXISTS = 'user_exists';
+        public const RULE_PWD_STRENGTH = 'pwd_strength';
 
         public array $errors = [];
 
@@ -106,6 +107,17 @@ namespace app\core
                             $this->addErrorForRule($attribute, self::RULE_USER_EXISTS);
                         }
                     }
+                    if($ruleName === self::RULE_PWD_STRENGTH)
+                    {
+                        $uppercase = preg_match('@[A-Z]@', $value);
+                        $lowercase = preg_match('@[a-z]@', $value);
+                        $number    = preg_match('@[0-9]@', $value);
+                        $specialChars = preg_match('@[^\w]@', $value);
+                        if(!$uppercase || !$lowercase || !$number || !$specialChars)
+                        {
+                            $this->addErrorForRule($attribute, self::RULE_PWD_STRENGTH);
+                        }
+                    }
                 }
             }
             return empty($this->errors);
@@ -135,7 +147,9 @@ namespace app\core
                 self::RULE_MAX => 'Max length of this field must be {max}',
                 self::RULE_MATCH => 'This field must be the same as {match}',
                 self::RULE_UNIQUE => 'Record with this {field} already exists',
-                self::RULE_USER_EXISTS => 'Invalid User selected, try another.'
+                self::RULE_USER_EXISTS => 'Invalid User selected, try another.',
+                self::RULE_PWD_STRENGTH => 'Password must contain at least 1 uppercase,
+                                        1 lowercase, 1 number and 1 special character.'
             ];
         }
 
--- BookITweb/models/User.php
+++ BookITweb/models/User.php
@@ -57,7 +57,9 @@ namespace app\models
                 'email' => [self::RULE_REQUIRED, self::RULE_EMAIL, [
                     self::RULE_UNIQUE, 'class' => self::class
                     ]],
-                'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8], [self::RULE_MAX, 'max' => 24]],
+                'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8],
+                              self::RULE_PWD_STRENGTH , [self::RULE_MAX, 'max' => 24]
+                              ],
                 'confirmPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']]
             ];
         }

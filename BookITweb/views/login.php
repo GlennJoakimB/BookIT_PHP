1c76a12feb434a377c67c735964e4eea9af48ecb
GlennJoakimB <89195051+GlennJoakimB@users.noreply.github.com>
2023-11-30 00:09:13 +0100

    Oppdatering til login-ui
    - Endringer på struktur
    - Lagt til styling
    - Lagt til style.css

--- BookITweb/views/login.php
+++ BookITweb/views/login.php
@@ -1,14 +1,37 @@
 <?php
-    /** @var $this \app\core\View */
-    $this->title = "Login";
+/** @var $this \app\core\View */
+$this->title = "Login";
 
-    /** @var $model \app\models\LoginForm */
+/** @var $model \app\models\LoginForm */
 ?>
 
 
-<h1>Login</h1>
-<?php $form = app\core\form\Form::begin('', "post") ?>
-<?php echo $form->field($model, 'email') ?>
-<?php echo $form->field($model, 'password')->passwordField() ?>
-<button type="submit" class="btn btn-primary">Submit</button>
-<?php echo app\core\form\Form::end() ?>
+<div class="row justify-content-center">
+    <div class="col-md-7 col-lg-5">
+        <div class="bg-white rounded shadow my-5">
+            <div class="img rounded-top" style="height: 100px;background-color: aqua;">
+                <div class="logo px-md-5 py-4" style="font-size: 2rem;">
+                    <div>BookIT</div>
+                    <iconify-icon icon="bx:book-bookmark"></iconify-icon>
+                </div>
+            </div>
+            <div class="login-wrap p-4 p-md-5">
+                <div class="d-flex">
+                    <div class="w-100">
+                        <h1 class="mb-4">Login</h1>
+                    </div>
+                </div>
+                <div id="form">
+                    <?php $form = app\core\form\Form::begin('', "post") ?>
+                    <?php echo $form->field($model, 'email') ?>
+                    <?php echo $form->field($model, 'password')->passwordField() ?>
+                    <button type="submit" class="btn btn-primary w-100 mt-2">Log in</button>
+                    <?php echo app\core\form\Form::end() ?>
+                </div>
+                <div class="text-center mt-3">
+                    Ingen bruker? <a href="/register">Register</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

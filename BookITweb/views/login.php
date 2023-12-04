51a75d4cac45662177a657fb2a1b114dde232baf
GlennJoakimB <89195051+GlennJoakimB@users.noreply.github.com>
2023-12-04 23:30:33 +0100

    Login ui cleanup
    - More styling
    - Link for redirecting between pages for login and registering

--- BookITweb/views/login.php
+++ BookITweb/views/login.php
@@ -6,11 +6,33 @@ $this->title = "Login";
 ?>
 
 
-<h1>Login</h1>
-
-<?php $form = app\core\form\Form::begin('', "post") ?>
-<?= $bannerError ?>
-<?php echo $form->field($model, 'email') ?>
-<?php echo $form->field($model, 'password')->passwordField() ?>
-<button type="submit" class="btn btn-primary">Submit</button>
-<?php echo app\core\form\Form::end() ?>
+<div class="row justify-content-center">
+    <div class="col-md-7 col-lg-5">
+        <div class="bg-white rounded shadow my-5">
+            <div class="img rounded-top d-flex p-4 fs-2 bg-primary-subtle">
+                <div class="logo d-flex">
+                    <div>BookIT</div>
+                    <iconify-icon class="d-flex align-items-center fs-2" icon="bx:book-bookmark"></iconify-icon>
+                </div>
+            </div>
+            <div class="login-wrap p-4">
+                <div class="d-flex">
+                    <div class="w-100">
+                        <h1 class="mb-4">Login</h1>
+                    </div>
+                </div>
+                <div id="form">
+                    <?php $form = app\core\form\Form::begin('', "post") ?>
+                    <?= $bannerError ?>
+                    <?php echo $form->field($model, 'email') ?>
+                    <?php echo $form->field($model, 'password')->passwordField() ?>
+                    <button type="submit" class="btn btn-primary w-100 mt-2">Log in</button>
+                    <?php echo app\core\form\Form::end() ?>
+                </div>
+                <div class="text-center mt-3">
+                    Don't have an account? <a href="/register">Register</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- BookITweb/views/register.php
+++ BookITweb/views/register.php
@@ -5,18 +5,41 @@
     /** @var $model \app\models\User */
 ?>
 
-<h1>Create an Account</h1>
-<?php $form = app\core\form\Form::begin('', "post")?>
-    <div class="row">         
-        <div class="col">
-            <?php echo $form->field($model, 'firstname') ?>
-        </div>
-        <div class="col">
-            <?php echo $form->field($model, 'lastname') ?>
+<div class="row justify-content-center">
+    <div class="col-md-7 col-lg-5">
+        <div class="bg-white rounded shadow my-5">
+            <div class="img rounded-top d-flex p-4 fs-2 bg-primary-subtle">
+                <div class="logo d-flex">
+                    <div>BookIT</div>
+                    <iconify-icon class="d-flex align-items-center fs-2" icon="bx:book-bookmark"></iconify-icon>
+                </div>
+            </div>
+            <div class="register-wrap p-4">
+                <div class="d-flex">
+                    <div class="w-100">
+                        <h1 class="mb-4">Create an Account</h1>
+                    </div>
+                </div>
+                <div id="form">
+                    <?php $form = app\core\form\Form::begin('', "post")?>
+                    <div class="row">
+                        <div class="col">
+                            <?php echo $form->field($model, 'firstname') ?>
+                        </div>
+                        <div class="col">
+                            <?php echo $form->field($model, 'lastname') ?>
+                        </div>
+                    </div>
+                    <?php echo $form->field($model, 'email') ?>
+                    <?php echo $form->field($model, 'password')->passwordField() ?>
+                    <?php echo $form->field($model, 'confirmPassword')->passwordField() ?>
+                    <button type="submit" class="btn btn-primary w-100 mt-2">Submit</button>
+                    <?php echo app\core\form\Form::end() ?>
+                </div>
+                <div class="text-center mt-3">
+                    Already have an account? <a href="/login">Sign in</a>
+                </div>
+            </div>
         </div>
     </div>
-    <?php echo $form->field($model, 'email') ?>
-    <?php echo $form->field($model, 'password')->passwordField() ?>
-    <?php echo $form->field($model, 'confirmPassword')->passwordField() ?>
-    <button type="submit" class="btn btn-primary">Submit</button>
-<?php echo app\core\form\Form::end() ?>
+</div>
